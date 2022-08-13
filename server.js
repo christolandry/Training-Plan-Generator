b3ad076ede985786360220428c87eb9e7fb8cd09
Christo Landry <Christo.Landry@gmail.com>
2022-08-13 13:54:22 -0400

    Add: Connect to MongoDb in server.js, npm init

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const express = require('express')
+const app = express() 
+const MongoClient = require('mongodb').MongoClient 
+const PORT = 2121 
+require('dotenv').config() 
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'training-plans'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
