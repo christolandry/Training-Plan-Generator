ea1c6de7c6ae0e8dd1cd17de2260bb06850c079a
Christo Landry <Christo.Landry@gmail.com>
2022-08-15 13:21:19 -0400

    Update: Minor changes, changed main.js to scheduler.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const express = require('express')
 const app = express() 
 const MongoClient = require('mongodb').MongoClient 
 const PORT = 2121 
@@ -11,7 +10,7 @@ let db,
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
-        console.log(`Connected to ${dbName} Database`)
+        console.log(`Connected to ${dbName} database`)
         db = client.db(dbName)
     })
 
@@ -23,11 +22,60 @@ app.use(express.json())
 
 //routes
 app.get('/',async (request, response)=>{  
-    const todoItems = await db.collection('test').find().toArray() 
-    response.render('index.ejs', { items: todoItems, left: itemsLeft }) 
-    .catch(error => console.error(error))
+    response.render('index.ejs') 
+    // .catch(error => console.error(error))
 })
 
+app.post('/generatePlan', (request, response) =>{
+    try{
+        const goalDistance = Number(request.body.goalDistance)
+        const planDuration = Number(request.body.planDuration)
+        const maxMileage = Number(request.body.maxMileage)
+        const maxLR = Number(request.body.maxLR)
+
+        let LR = generateLongRuns(planDuration, goalDistance, maxLR)
+        
+        
+        
+
+
+
+
+        response.render('plan.ejs', {distance: goalDistance, longRuns: LR})
+    }catch(error){
+        console.log(error)
+    }
+    
+})
+
+
+function generateLongRuns(duration, race, maxLR){
+    let longRuns = Array(duration).fill(0)
+    console.log(`longruns: ${longRuns}`)
+    console.log(`duration: ${duration}  race: ${race}`)   
+    if(duration === 16 && race === 26){
+        longRuns[15] = race
+        longRuns[14] = Math.floor(maxLR * 0.6)
+        longRuns[13] = Math.floor(maxLR * 0.8)
+        longRuns[12] = maxLR
+        longRuns[11] = Math.floor(maxLR * 0.85)
+        longRuns[10] = maxLR
+        longRuns[9] = Math.floor(maxLR * 0.85)
+        longRuns[8] = maxLR
+        longRuns[7] = longRuns[9]-1
+        longRuns[6] = longRuns[8]-1
+        longRuns[5] = longRuns[7]
+        longRuns[4] = longRuns[6]-1
+        longRuns[3] = longRuns[4]-2
+        longRuns[2] = longRuns[3]-2
+        longRuns[1] = longRuns[2]-2
+        longRuns[0] = longRuns[1]-2
+        console.log("hello")
+    }
+    console.log(`longruns: ${longRuns}`)
+    console.log(`length: ${longRuns.length}`)
+    return longRuns
+}
 
 
 
