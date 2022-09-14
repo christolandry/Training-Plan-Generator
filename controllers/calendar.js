048bc5b96fdcfdd976860200fe8b8202831c668f
Christo Landry <Christo.Landry@gmail.com>
2022-09-14 12:46:49 -0400

    Add: Ability to put in a custom Monday to start the training plan

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -55,7 +55,7 @@ module.exports = {
 
 function generateStartDate(enteredDate){
     let startDate = new Date(enteredDate)
-    startDate.setDate(startDate.getDate() - startDate.getDay())
+    startDate.setDate(startDate.getUTCDate() - startDate.getDay())
     return startDate
 }
 
