7375004f40aefb75a32af4f5afca05d9a11b670b
Christo Landry <Christo.Landry@gmail.com>
2022-09-14 14:58:22 -0400

    Update: Fixed bug where a logged out user didn't have a path in the / controller

--- controllers/home.js
+++ controllers/home.js
@@ -4,10 +4,6 @@ const TrainingPlan = require("../models/TrainingPlan");
 module.exports = {    
     getIndex: async (req,res)=>{
         try{
-            // console.log("About to assign plan")
-            // let plan = await TrainingPlan.findOne({userId: req.user.id})
-            // console.log(plan)
-            // if()
             if(req.user) return await TrainingPlan.findOne({userId: req.user.id}) ? res.redirect('/calendar') : res.render('index.ejs')            
             res.render('index.ejs')
         }catch(error){
